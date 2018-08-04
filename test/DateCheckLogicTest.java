e17b10db0458229a31aa4e9121f4036169723127
overdrivep <kazuki.4510@gmail.com>
2018-08-04 20:34:41 +0900

    初回作成

---
+++ test/DateCheckLogicTest.java
@@ -0,0 +1,22 @@
+package border.logic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DateCheckLogicTest {
+
+	@Test
+	void testAcquireServerDate() {
+
+	}
+
+	@Test
+	void testAcquireEventDate() {
+
+	}
+
+	void testGetDay() {
+
+	}
+}
