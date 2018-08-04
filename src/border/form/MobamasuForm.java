5b7e68f253cb2709eba5f9bf969d0639edc9f710
overdrivep <kazuki.4510@gmail.com>
2018-08-04 20:59:07 +0900

    初回作成

---
+++ src/border/form/MobamasuForm.java
@@ -0,0 +1,555 @@
+package border.form;
+
+public class MobamasuForm {
+	
+	private String eventName;
+	
+	private String day1Point_1;
+	
+	private String day1Point_100;
+	
+	private String day1Point_200;
+	
+	private String day1Point_1000;
+	
+	private String day1Point_1500;
+	
+	private String day1Point_2000;
+
+	private String day2Point_1;
+	
+	private String day2Point_100;
+	
+	private String day2Point_200;
+	
+	private String day2Point_1000;
+	
+	private String day2Point_1500;
+	
+	private String day2Point_2000;
+	
+	private String day3Point_1;
+	
+	private String day3Point_100;
+	
+	private String day3Point_200;
+	
+	private String day3Point_1000;
+	
+	private String day3Point_1500;
+	
+	private String day3Point_2000;
+	
+	private String day4Point_1;
+	
+	private String day4Point_100;
+	
+	private String day4Point_200;
+	
+	private String day4Point_1000;
+	
+	private String day4Point_1500;
+	
+	private String day4Point_2000;
+
+	private String day5Point_1;
+	
+	private String day5Point_100;
+	
+	private String day5Point_200;
+	
+	private String day5Point_1000;
+	
+	private String day5Point_1500;
+	
+	private String day5Point_2000;
+	
+	private String day6Point_1;
+	
+	private String day6Point_100;
+	
+	private String day6Point_200;
+	
+	private String day6Point_1000;
+	
+	private String day6Point_1500;
+	
+	private String day6Point_2000;
+	
+	private String day7Point_1;
+	
+	private String day7Point_100;
+	
+	private String day7Point_200;
+	
+	private String day7Point_1000;
+	
+	private String day7Point_1500;
+	
+	private String day7Point_2000;
+	
+	private String day8Point_1;
+	
+	private String day8Point_100;
+	
+	private String day8Point_200;
+	
+	private String day8Point_1000;
+	
+	private String day8Point_1500;
+	
+	private String day8Point_2000;
+	
+	private String day9Point_1;
+	
+	private String day9Point_100;
+	
+	private String day9Point_200;
+	
+	private String day9Point_1000;
+	
+	private String day9Point_1500;
+	
+	private String day9Point_2000;
+
+	public String getEventName() {
+		return eventName;
+	}
+
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+
+	public String getDay1Point_1() {
+		return day1Point_1;
+	}
+
+	public void setDay1Point_1(String day1Point_1) {
+		this.day1Point_1 = day1Point_1;
+	}
+
+	public String getDay1Point_100() {
+		return day1Point_100;
+	}
+
+	public void setDay1Point_100(String day1Point_100) {
+		this.day1Point_100 = day1Point_100;
+	}
+
+	public String getDay1Point_200() {
+		return day1Point_200;
+	}
+
+	public void setDay1Point_200(String day1Point_200) {
+		this.day1Point_200 = day1Point_200;
+	}
+
+	public String getDay1Point_1000() {
+		return day1Point_1000;
+	}
+
+	public void setDay1Point_1000(String day1Point_1000) {
+		this.day1Point_1000 = day1Point_1000;
+	}
+
+	public String getDay1Point_1500() {
+		return day1Point_1500;
+	}
+
+	public void setDay1Point_1500(String day1Point_1500) {
+		this.day1Point_1500 = day1Point_1500;
+	}
+
+	public String getDay1Point_2000() {
+		return day1Point_2000;
+	}
+
+	public void setDay1Point_2000(String day1Point_2000) {
+		this.day1Point_2000 = day1Point_2000;
+	}
+
+	public String getDay2Point_1() {
+		return day2Point_1;
+	}
+
+	public void setDay2Point_1(String day2Point_1) {
+		this.day2Point_1 = day2Point_1;
+	}
+
+	public String getDay2Point_100() {
+		return day2Point_100;
+	}
+
+	public void setDay2Point_100(String day2Point_100) {
+		this.day2Point_100 = day2Point_100;
+	}
+
+	public String getDay2Point_200() {
+		return day2Point_200;
+	}
+
+	public void setDay2Point_200(String day2Point_200) {
+		this.day2Point_200 = day2Point_200;
+	}
+
+	public String getDay2Point_1000() {
+		return day2Point_1000;
+	}
+
+	public void setDay2Point_1000(String day2Point_1000) {
+		this.day2Point_1000 = day2Point_1000;
+	}
+
+	public String getDay2Point_1500() {
+		return day2Point_1500;
+	}
+
+	public void setDay2Point_1500(String day2Point_1500) {
+		this.day2Point_1500 = day2Point_1500;
+	}
+
+	public String getDay2Point_2000() {
+		return day2Point_2000;
+	}
+
+	public void setDay2Point_2000(String day2Point_2000) {
+		this.day2Point_2000 = day2Point_2000;
+	}
+
+	public String getDay3Point_1() {
+		return day3Point_1;
+	}
+
+	public void setDay3Point_1(String day3Point_1) {
+		this.day3Point_1 = day3Point_1;
+	}
+
+	public String getDay3Point_100() {
+		return day3Point_100;
+	}
+
+	public void setDay3Point_100(String day3Point_100) {
+		this.day3Point_100 = day3Point_100;
+	}
+
+	public String getDay3Point_200() {
+		return day3Point_200;
+	}
+
+	public void setDay3Point_200(String day3Point_200) {
+		this.day3Point_200 = day3Point_200;
+	}
+
+	public String getDay3Point_1000() {
+		return day3Point_1000;
+	}
+
+	public void setDay3Point_1000(String day3Point_1000) {
+		this.day3Point_1000 = day3Point_1000;
+	}
+
+	public String getDay3Point_1500() {
+		return day3Point_1500;
+	}
+
+	public void setDay3Point_1500(String day3Point_1500) {
+		this.day3Point_1500 = day3Point_1500;
+	}
+
+	public String getDay3Point_2000() {
+		return day3Point_2000;
+	}
+
+	public void setDay3Point_2000(String day3Point_2000) {
+		this.day3Point_2000 = day3Point_2000;
+	}
+
+	public String getDay4Point_1() {
+		return day4Point_1;
+	}
+
+	public void setDay4Point_1(String day4Point_1) {
+		this.day4Point_1 = day4Point_1;
+	}
+
+	public String getDay4Point_100() {
+		return day4Point_100;
+	}
+
+	public void setDay4Point_100(String day4Point_100) {
+		this.day4Point_100 = day4Point_100;
+	}
+
+	public String getDay4Point_200() {
+		return day4Point_200;
+	}
+
+	public void setDay4Point_200(String day4Point_200) {
+		this.day4Point_200 = day4Point_200;
+	}
+
+	public String getDay4Point_1000() {
+		return day4Point_1000;
+	}
+
+	public void setDay4Point_1000(String day4Point_1000) {
+		this.day4Point_1000 = day4Point_1000;
+	}
+
+	public String getDay4Point_1500() {
+		return day4Point_1500;
+	}
+
+	public void setDay4Point_1500(String day4Point_1500) {
+		this.day4Point_1500 = day4Point_1500;
+	}
+
+	public String getDay4Point_2000() {
+		return day4Point_2000;
+	}
+
+	public void setDay4Point_2000(String day4Point_2000) {
+		this.day4Point_2000 = day4Point_2000;
+	}
+
+	public String getDay5Point_1() {
+		return day5Point_1;
+	}
+
+	public void setDay5Point_1(String day5Point_1) {
+		this.day5Point_1 = day5Point_1;
+	}
+
+	public String getDay5Point_100() {
+		return day5Point_100;
+	}
+
+	public void setDay5Point_100(String day5Point_100) {
+		this.day5Point_100 = day5Point_100;
+	}
+
+	public String getDay5Point_200() {
+		return day5Point_200;
+	}
+
+	public void setDay5Point_200(String day5Point_200) {
+		this.day5Point_200 = day5Point_200;
+	}
+
+	public String getDay5Point_1000() {
+		return day5Point_1000;
+	}
+
+	public void setDay5Point_1000(String day5Point_1000) {
+		this.day5Point_1000 = day5Point_1000;
+	}
+
+	public String getDay5Point_1500() {
+		return day5Point_1500;
+	}
+
+	public void setDay5Point_1500(String day5Point_1500) {
+		this.day5Point_1500 = day5Point_1500;
+	}
+
+	public String getDay5Point_2000() {
+		return day5Point_2000;
+	}
+
+	public void setDay5Point_2000(String day5Point_2000) {
+		this.day5Point_2000 = day5Point_2000;
+	}
+
+	public String getDay6Point_1() {
+		return day6Point_1;
+	}
+
+	public void setDay6Point_1(String day6Point_1) {
+		this.day6Point_1 = day6Point_1;
+	}
+
+	public String getDay6Point_100() {
+		return day6Point_100;
+	}
+
+	public void setDay6Point_100(String day6Point_100) {
+		this.day6Point_100 = day6Point_100;
+	}
+
+	public String getDay6Point_200() {
+		return day6Point_200;
+	}
+
+	public void setDay6Point_200(String day6Point_200) {
+		this.day6Point_200 = day6Point_200;
+	}
+
+	public String getDay6Point_1000() {
+		return day6Point_1000;
+	}
+
+	public void setDay6Point_1000(String day6Point_1000) {
+		this.day6Point_1000 = day6Point_1000;
+	}
+
+	public String getDay6Point_1500() {
+		return day6Point_1500;
+	}
+
+	public void setDay6Point_1500(String day6Point_1500) {
+		this.day6Point_1500 = day6Point_1500;
+	}
+
+	public String getDay6Point_2000() {
+		return day6Point_2000;
+	}
+
+	public void setDay6Point_2000(String day6Point_2000) {
+		this.day6Point_2000 = day6Point_2000;
+	}
+
+	public String getDay7Point_1() {
+		return day7Point_1;
+	}
+
+	public void setDay7Point_1(String day7Point_1) {
+		this.day7Point_1 = day7Point_1;
+	}
+
+	public String getDay7Point_100() {
+		return day7Point_100;
+	}
+
+	public void setDay7Point_100(String day7Point_100) {
+		this.day7Point_100 = day7Point_100;
+	}
+
+	public String getDay7Point_200() {
+		return day7Point_200;
+	}
+
+	public void setDay7Point_200(String day7Point_200) {
+		this.day7Point_200 = day7Point_200;
+	}
+
+	public String getDay7Point_1000() {
+		return day7Point_1000;
+	}
+
+	public void setDay7Point_1000(String day7Point_1000) {
+		this.day7Point_1000 = day7Point_1000;
+	}
+
+	public String getDay7Point_1500() {
+		return day7Point_1500;
+	}
+
+	public void setDay7Point_1500(String day7Point_1500) {
+		this.day7Point_1500 = day7Point_1500;
+	}
+
+	public String getDay7Point_2000() {
+		return day7Point_2000;
+	}
+
+	public void setDay7Point_2000(String day7Point_2000) {
+		this.day7Point_2000 = day7Point_2000;
+	}
+
+	public String getDay8Point_1() {
+		return day8Point_1;
+	}
+
+	public void setDay8Point_1(String day8Point_1) {
+		this.day8Point_1 = day8Point_1;
+	}
+
+	public String getDay8Point_100() {
+		return day8Point_100;
+	}
+
+	public void setDay8Point_100(String day8Point_100) {
+		this.day8Point_100 = day8Point_100;
+	}
+
+	public String getDay8Point_200() {
+		return day8Point_200;
+	}
+
+	public void setDay8Point_200(String day8Point_200) {
+		this.day8Point_200 = day8Point_200;
+	}
+
+	public String getDay8Point_1000() {
+		return day8Point_1000;
+	}
+
+	public void setDay8Point_1000(String day8Point_1000) {
+		this.day8Point_1000 = day8Point_1000;
+	}
+
+	public String getDay8Point_1500() {
+		return day8Point_1500;
+	}
+
+	public void setDay8Point_1500(String day8Point_1500) {
+		this.day8Point_1500 = day8Point_1500;
+	}
+
+	public String getDay8Point_2000() {
+		return day8Point_2000;
+	}
+
+	public void setDay8Point_2000(String day8Point_2000) {
+		this.day8Point_2000 = day8Point_2000;
+	}
+
+	public String getDay9Point_1() {
+		return day9Point_1;
+	}
+
+	public void setDay9Point_1(String day9Point_1) {
+		this.day9Point_1 = day9Point_1;
+	}
+
+	public String getDay9Point_100() {
+		return day9Point_100;
+	}
+
+	public void setDay9Point_100(String day9Point_100) {
+		this.day9Point_100 = day9Point_100;
+	}
+
+	public String getDay9Point_200() {
+		return day9Point_200;
+	}
+
+	public void setDay9Point_200(String day9Point_200) {
+		this.day9Point_200 = day9Point_200;
+	}
+
+	public String getDay9Point_1000() {
+		return day9Point_1000;
+	}
+
+	public void setDay9Point_1000(String day9Point_1000) {
+		this.day9Point_1000 = day9Point_1000;
+	}
+
+	public String getDay9Point_1500() {
+		return day9Point_1500;
+	}
+
+	public void setDay9Point_1500(String day9Point_1500) {
+		this.day9Point_1500 = day9Point_1500;
+	}
+
+	public String getDay9Point_2000() {
+		return day9Point_2000;
+	}
+
+	public void setDay9Point_2000(String day9Point_2000) {
+		this.day9Point_2000 = day9Point_2000;
+	}
+}
+
