c4fd931c84f1b4d5edd91c12c09a02a7996134f7
watanabeg <41868610+watanabeg@users.noreply.github.com>
2018-08-25 17:45:52 +0900

    logic及びtestの構成を変更

--- src/test/DateCheckLogicTest.java
+++ src/test/DateCheckLogicTest.java
@@ -1,77 +1,15 @@
 package test;
 
-import java.text.SimpleDateFormat;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class DateCheckLogicTest {
+class DateCheckLogicTestTest {
 
 	@Test
-	//	今日の日付
-	String testAcquireServerDate() {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyMMdd");
-		return sdf.format(new java.util.Date());
+	final void testTestGetDay() {
+		DateCheckLogicTest dc = new DateCheckLogicTest();
+		assertEquals(2, dc.testGetDay());
 	}
 
-	@Test
-	//	イベント開始日
-	String testAcquireEventDate() {
-		//TODO SQL作成後差し替え
-
-		String startDay = null;
-
-
-
-		//SQL部
-		//appendでSQLを作る?
-		//構造決定次第
-		/*
-		 *   //変数定義
-        Connection conn = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        //DB接続情報を設定する
-        String path = ;  //接続パス
-        String id = ;  //ログインID
-        String pw = ;  //ログインパスワード
-
-		StringBuilder sql = new StringBuilder("");
-		sql.append("SELECT ");
-		sql.append("");//フィールド。イベント開始日
-		sql.append("FROM ");
-		sql.append("");//テーブル名。
-		sql.append("WHERE");
-		sql.append("[イベントを判別できる何か] = ?");//条件。該当イベントを指定
-
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		//DBへのコネクションを作成する
-		conn = DriverManager.getConnection(path, id, pw);
-		//実行するSQL文とパラメータを指定する
-		ps = conn.prepareStatement(sql);
-		ps.setString(1, "");パラメータにイベント判別要素をset
-
-		rs = ps.executeQuery();
-
-		startDay = rs;
-		*/
-
-		//とりあえず固定値
-		startDay = "180804";
-
-		return startDay;
-
-	}
-
-	//	開催から何日目
-	int testGetDay() {
-		String today = testAcquireServerDate();
-
-		String startDay = testAcquireEventDate();
-
-        int day = Integer.parseInt(today) - Integer.parseInt(startDay)+1;
-
-        return day;
-
-	}
 }
